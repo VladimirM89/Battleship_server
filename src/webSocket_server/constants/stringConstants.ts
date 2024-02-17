7799d2c334007b0780b93932b94428024240d494
Vladimir <v.milashevsky@gmail.com>
2024-02-17 16:26:03 +0300

    feat: add add constants, user model

---
+++ src/webSocket_server/constants/stringConstants.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const WEBSOCKET_START_TEXT = "Websocket is created at the address:";
--- src/webSocket_server/index.ts
+++ src/webSocket_server/index.ts
@@ -1,7 +1,9 @@
 import { WebSocketServer } from "ws";
+import { WEBSOCKET_HOST, WEBSOCKET_PORT } from "./constants/webSocketConstants";
+import { WEBSOCKET_START_TEXT } from "./constants/stringConstants";
 
-const wss = new WebSocketServer({ port: 3000 });
-console.log("Server WS created");
+const wss = new WebSocketServer({ port: WEBSOCKET_PORT, host: WEBSOCKET_HOST });
+console.log(`${WEBSOCKET_START_TEXT} ${WEBSOCKET_HOST}: ${WEBSOCKET_PORT}`);
 
 wss.on("connection", (ws) => {
   ws.on("error", console.error);
